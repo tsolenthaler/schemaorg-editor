0e6e30146f09c7a54ab3e022a43192ad6e982198
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-01-25 11:23:17 +0100

    Add papaparse library and implement code formatting

--- src/app/components/edit/index.tsx
+++ src/app/components/edit/index.tsx
@@ -1,12 +1,20 @@
 'use client'
 
 import MonacoEditor from '@monaco-editor/react';
+import { useRef, useEffect, useState } from 'react';
+import Papa from 'papaparse';
 
 const code = `{
   "some": "json"
 }`;
 
 const Edit = (_: any) => {
+    const editorRef = useRef(null);
+    const [completionItems, setCompletionItems] = useState([]);
+  
+    const provideCompletionItems = () => {
+      return { suggestions: completionItems };
+    };
   
     function onMount(editor: any, monaco: any) {
         const modelUri = "https://tsolenthaler.github.io/schemaorg-editor/examples/hotel.jsonld"
@@ -15,31 +23,34 @@ const Edit = (_: any) => {
     "@type": "Place"
 }`, "json", monaco.Uri.parse(modelUri)
         )
+
+        fetch('https://schema.org/version/latest/schemaorg-current-https-properties.csv')
+        .then(response => response.text())
+        .then(data => {
+          const results = Papa.parse(data, { header: true });
+          
+          setCompletionItems(results.data.map(item => ({
+            label: item.propertyID,
+            kind: monaco.languages.CompletionItemKind.Property,
+            insertText: item.propertyID,
+            detail: item.description,
+          })));
+        });
+        
+        monaco.languages.registerCompletionItemProvider('json', { provideCompletionItems });
         editor.setModel(model)
-    
-        monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
-          ...monaco.languages.json.jsonDefaults.diagnosticOptions,
-          allowComments: true,
-          schemas: [
-            {
-              //uri: "foo://myapp/segment/type",
-              fileMatch: ["*.json"],
-              schema: {
-                type: "object",
-                properties: {
-                  type: {
-                    enum: ["v1", "v2"],
-                  }
-                },
-              },
-            }
-          ],
-          validate: true
-        })
+        editorRef.current = editor;
+    }
+
+    const formatCode = () => {
+      if (editorRef.current) {
+        editorRef.current.getAction('editor.action.formatDocument').run()
+      }
     }
 
     return(
         <div>
+           <button onClick={formatCode}>Format Code</button>
             <MonacoEditor
                 height="100vh" // By default, it fully fits with its parent
                 theme="dark"
